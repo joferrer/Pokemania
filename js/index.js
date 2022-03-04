1aa0dbb6a2bacee9365a3adc9f89336524414b5b
Jeison Ferrer <jeison120120@hotmail.com>
2022-03-04 16:07:55 -0500

    Documentando los métodos

--- js/index.js
+++ js/index.js
@@ -1,13 +1,23 @@
 
+//Url de la API PokeApi
 const urlApi = 'https://pokeapi.co/api/v2/';
 
+/**
+ * Se ejecuta al inciar la pagina. Lee la url buscando parametros y los manda a cargar. 
+ */
 function pokeApi (){
     let urlParams = new URLSearchParams(location.search); 
     console.log('param: '+urlParams.get('pokemonName'));
     buscarPokemon(urlParams.get('pokemonName'));
 }
+/**
+ * Carga la info del pokemon pasado por parametro. Si no hay parametro o es null, el pokemon que se carga es Pikachu
+ * @param {string} pokemon 
+ */
 function buscarPokemon(pokemon='pikachu'){
-    
+    if(pokemon == null ){
+        pokemon = 'pikachu';
+    }
     let urlPokemon = urlApi+'pokemon/'+pokemon;
     console.log('hola '+ pokemon);
 
