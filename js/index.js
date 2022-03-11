cbae2425968e3df5280e50d5c556ae29419035bd
Jeison Ferrer <jeison120120@hotmail.com>
2022-03-10 19:02:07 -0500

    1. Se muestra una card con las caracteristicas basicas del pokemon buscado

--- js/index.js
+++ js/index.js
@@ -23,10 +23,35 @@ function buscarPokemon(pokemon='pikachu'){
     let urlPokemon = urlApi+'pokemon/'+pokemon;
     console.log('hola '+ pokemon);
 
-    fetch(urlPokemon).then(response =>response.json()).then(response=>{
+    fetch(urlPokemon).then(response =>response.json()).then( async response=>{
         console.log(response.forms[0].name);
-        document.getElementById('pokeinfo').innerHTML+=response.forms[0].name;
+        document.getElementsByClassName('card-title')[0].innerHTML+=response.forms[0].name;
         document.getElementsByTagName('img')[0].src=response.sprites.front_default;
+        console.log(response.types[0].type.name);
+
+        document.getElementsByClassName('card-text')[0].innerHTML = await descripcion(response);
+        document.getElementsByClassName('card-text')[1].innerHTML+=" "+response.types[0].type.name;
+
+        document.getElementsByClassName('list-group-item')[0].innerHTML+= " "+ response.stats[0].base_stat;
+        document.getElementsByClassName('list-group-item')[1].innerHTML+= " "+ response.stats[1].base_stat;
+        document.getElementsByClassName('list-group-item')[2].innerHTML+= " "+ response.stats[2].base_stat;
+        document.getElementsByClassName('list-group-item')[3].innerHTML+= " "+ response.stats[3].base_stat;
+
     });
 
+}
+const descripcion = async (response)=>{
+    console.log(response.id);
+    try{
+    let resp =await fetch('https://pokeapi.co/api/v2/characteristic/'+response.id);
+    let jsonD = await resp.json();
+    let descripcion = jsonD.descriptions[5].description;
+    console.log(descripcion);
+    return descripcion;
+    }
+    catch (e) {
+        return  'Sin descripción :('
+    }
+    
+    
 }
